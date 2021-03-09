791f1c62dfb1ff5a8ea6d4767d1229cd1d8b0413
Samantha Jordan <shinyguns@gmail.com>
2021-03-09 16:47:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your code here!
 //remove main
-var element = document.getElementById("H1")
+var element = document.getElementById("main")
 element.parentNode.removeChild(element)
 
-//create h1 with id
+//create h1 with id n html
 let newHeader = document.createElement("h1");
 newHeader.innerHTML = "Samantha is the champion"
-newHeader.id = victory
+newHeader.id = "victory"
